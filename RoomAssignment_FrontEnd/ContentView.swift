f2a7f205558bb6c8cf53a38fe2c828d318cee390
NatalmanNah <87883622+NatalmanNah@users.noreply.github.com>
2022-03-12 18:27:41 -0600

    Initial Commit

---
+++ RoomAssignment_FrontEnd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RoomAssignment_FrontEnd
+//
+//  Created by Natalman Nahm on 3/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RoomAssignment_FrontEnd/RoomAssignment_FrontEndApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomAssignment_FrontEndApp.swift
+//  RoomAssignment_FrontEnd
+//
+//  Created by Natalman Nahm on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RoomAssignment_FrontEndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
